5b6afc14c019dbe355964a3ce821ceabf01ae433
Troy Calandra <troycalandra@Troys-MacBook-Pro.local>
2021-02-25 18:31:50 -0500

    Migrate to new metamask connection

--- src/app.js
+++ src/app.js
@@ -3,54 +3,9 @@ App = {
   contracts: {},
 
   load: async () => {
-    await App.loadWeb3()
+    await App.loadAccount()
+    await App.loadContract()
     await App.render()
-  },
-
-  // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
-  loadWeb3: async () => {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      window.alert("Please connect to Metamask.")
-    }
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      console.log('inside first')
-      await App.loadContract()
-      await App.loadAccount()
-
-      try {
-        const transactionHash = await ethereum.request({
-          method: 'eth_sendTransaction',
-          params: [
-            {
-              to: App.todoList.address,
-              'from': App.account,
-              value: '0x00',
-              // And so on...
-            },
-          ],
-        });
-        // Handle the result
-        console.log(transactionHash);
-      } catch (error) {
-        // User denied account access...
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      console.log('inside second')
-      App.web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
-    }
-    // Non-dapp browsers...
-    else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
   },
 
   loadAccount: async () => {
@@ -63,11 +18,12 @@ App = {
     // Create a JavaScript version of the smart contract
     const todoList = await $.getJSON('TodoList.json')
     App.contracts.TodoList = TruffleContract(todoList)
-    App.contracts.TodoList.setProvider(App.web3Provider)
+    App.contracts.TodoList.defaults({from: App.account})
+
+    App.contracts.TodoList.setProvider(web3.currentProvider)
 
     // Hydrate the smart contract with values from the blockchain
     App.todoList = await App.contracts.TodoList.deployed()
-    console.log(App.todoList.address)
   },
 
   render: async () => {
@@ -91,9 +47,7 @@ App = {
 
   renderTasks: async () => {
     // Load the total task count from the blockchain
-    console.log(App.todoList)
     const taskCount = await App.todoList.taskCount()
-    console.log(taskCount)
     const $taskTemplate = $('.taskTemplate')
 
     // Render out each task with a new task template
